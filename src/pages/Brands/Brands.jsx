0dc4e4cc31fdc5b95f7bd8f1614d59f9dcc0bc4f
Danulo <danilyanishevski2001@gmail.com>
2024-07-09 00:40:38 +0300

    add Link

--- src/pages/Brands/Brands.jsx
+++ src/pages/Brands/Brands.jsx
@@ -1,8 +1,12 @@
 import BrandsList from "../../components/BrandsList/BrandsList";
+import css from "./Brands.module.css";
+import Header from "../../components/Header/Header";
 
 const Brands = () => {
   return (
-    <div>
+    <div className={css.qqq}>
+      <Header />
+      <h1 className={css.title}>Бренди</h1>
       <BrandsList />
     </div>
   );
