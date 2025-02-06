ec87196fd524fed31a375ed65508fc44b35473c7
danilkagugu <142350737+danilkagugu@users.noreply.github.com>
2025-02-06 17:36:08 +0200

    filters

---
+++ src/hooks/useCatalogFilters.js
@@ -0,0 +1,48 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useCallback } from "react";
+
+export const useCatalogFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  // Парсимо фільтри з URL
+  const filters = {
+    brands: searchParams.get("brand")?.split(",") || [],
+    categories: searchParams.get("category")?.split(",") || [],
+    price: searchParams.get("price")
+      ? searchParams.get("price").split("-").map(Number)
+      : null,
+    page: Number(searchParams.get("page")) || 1,
+  };
+
+  // Функція для оновлення фільтрів і синхронізації з URL
+  const updateFilters = useCallback(
+    (newFilters) => {
+      const params = new URLSearchParams();
+      if (newFilters.price) {
+        params.append("price", `${newFilters.price[0]}-${newFilters.price[1]}`);
+      }
+
+      if (newFilters.brands.length > 0) {
+        params.append("brand", newFilters.brands.join(","));
+      }
+
+      if (newFilters.categories.length > 0) {
+        params.append("category", newFilters.categories.join(","));
+      }
+
+       
+
+      if (newFilters.page > 1) {
+        params.append("page", newFilters.page);
+      }
+
+      // Оновлення URL
+      setSearchParams(params);
+      navigate(`/catalog/filter?${params.toString()}`, { replace: true });
+    },
+    [setSearchParams, navigate]
+  );
+
+  return { filters, updateFilters };
+};
